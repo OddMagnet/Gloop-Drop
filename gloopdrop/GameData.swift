fa72167a318efb85d20f17a455a1f6840c2f2e4b
OddMagnet <mibruenen@gmail.com>
2020-09-17 18:53:51 +0200

    Add GameData storage

---
+++ gloopdrop/GameData.swift
@@ -0,0 +1,69 @@
+//
+//  GameData.swift
+//  gloopdrop
+//
+//  Created by Michael Brünen on 17.09.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import Foundation
+
+class GameData: NSObject, Codable {
+    // MARK: - Properties
+    let saveDataFileName = "gamedata.json"
+    var freeContinues: Int = 1 {
+        didSet {
+            saveDataWithFileName(saveDataFileName)
+        }
+    }
+
+    static let shared: GameData = {
+        let instance = GameData()
+        instance.setUpObservers()
+        return instance
+    }()
+
+    // MARK: - Init
+    private override init() {}
+
+    // MARK: - Notification Handlers
+    func setUpObservers() {
+        // TODO: Add observers
+    }
+    // MARK: - Save and load locally stored data
+    func saveDataWithFileName(_ filename: String) {
+        let fullPath = getDocumentsDirectory().appendingPathComponent(filename)
+
+        do {
+            let data = try PropertyListEncoder().encode(self)
+            let dataFile = try NSKeyedArchiver.archivedData(withRootObject: data,
+                                                            requiringSecureCoding: true)
+            try dataFile.write(to: fullPath)
+        } catch {
+            print("Couldn't write game data")
+        }
+    }
+
+    func loadDataWithFileName(_ filename: String) {
+        let fullPath = getDocumentsDirectory().appendingPathComponent(filename)
+
+        do {
+            let contents = try Data(contentsOf: fullPath)
+            if let data = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(contents) as? Data {
+                let gameData = try PropertyListDecoder().decode(GameData.self, from: data)
+
+                freeContinues = gameData.freeContinues
+            }
+        } catch {
+            print("Couldn't load game data")
+        }
+    }
+
+    // MARK: - Helper
+    fileprivate func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory,
+                                             in: .userDomainMask)
+        return paths[0]
+    }
+
+}
