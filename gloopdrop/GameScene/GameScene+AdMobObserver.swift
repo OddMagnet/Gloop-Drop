31b62d414637997b7e0d0e05e66ebe958c047e13
OddMagnet <mibruenen@gmail.com>
2020-09-18 16:53:21 +0200

    Add Documentation

--- gloopdrop/GameScene/GameScene+AdMobObserver.swift
+++ gloopdrop/GameScene/GameScene+AdMobObserver.swift
@@ -11,8 +11,8 @@ import GoogleMobileAds
 
 /// This part of the GameScene class contains the setup for AdMob observers
 extension GameScene {
+    /// Adds notification observers for the `.userDidEarnReward`, `.adDidOrWillPresent`and `.adDidOrWillDismiss` notifications
     func setUpAdMobObservers() {
-        // Add notification observers
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(self.userDidEarnReward(_:)),
                                                name: .userDidEarnReward,
@@ -27,15 +27,19 @@ extension GameScene {
                                                object: nil)
     }
 
+    /// Handles the `.userDidEarnReward` notification
+    /// - Parameter reward: the reward given, currently unused
     @objc func userDidEarnReward(_ reward: GADAdReward) {
         numberOfFreeContinues += 1
     }
 
+    /// Handles the `.adDidOrWillPresent` notification
     @objc func adDidOrWillPresent() {
         audioEngine.mainMixerNode.outputVolume = 0.0
         watchAdButton.alpha = 0.0
     }
 
+    /// Handles the `.adDidOrWillDismiss` notification
     @objc func adDidOrWillDismiss() {
         audioEngine.mainMixerNode.outputVolume = 1.0
     }
